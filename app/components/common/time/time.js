ee107039c2cbb7ce14de82edbe8940bf9692c58d
Ścibior Arkadiusz <ascibior@kbj.com.pl>
2020-06-04 09:53:22 +0200

    Czas: zmiana obslugi czasu przez momentjs oraz angielska nazwa dnia.

--- app/components/common/time/time.js
+++ app/components/common/time/time.js
@@ -5,30 +5,30 @@ app.directive('currentDate', function () {
 		template: [
             '<div class="row">',
                 '<div class="col-md-12">',
-                    '<p class="date"> {{dayName.text}}, {{date}}</p>',
+                    '<p class="date"> {{dayName}}, {{date}}</p>',
                 '</div>',
             '</div>'
 		].join(''),
 		controller: ["$scope", "$interval", "$filter", function ($scope, $interval, $filter) {
 
             var getFullDate = function() {
-                var date = $filter('date')(new Date(), 'dd.MM.yyyy');
-                var time = $filter('date')(new Date(), 'HH:mm:ss');
+                var date = moment().format('DD.MM.YYYY');
+                var time =  moment().format('HH:mm:ss');
 
                 return date + ", " + time;
             }
-            var days = [
-                {value:1, text:'Poniedziałek'},
-                {value:2, text:'Wtorek'},
-                {value:3, text:'Środa'},
-                {value:4, text:'Czwartek'},
-                {value:5, text:'Piątek'},
-                {value:6, text:'Sobota'},
-                {value:7, text:'Niedziela'}
-            ];
+            // var days = [
+            //     {value:1, text:'Poniedziałek'},
+            //     {value:2, text:'Wtorek'},
+            //     {value:3, text:'Środa'},
+            //     {value:4, text:'Czwartek'},
+            //     {value:5, text:'Piątek'},
+            //     {value:6, text:'Sobota'},
+            //     {value:7, text:'Niedziela'}
+            // ];
             $interval(function() {
                 $scope.date = getFullDate()
-                $scope.dayName = days[new Date().getDay()-1];
+                $scope.dayName = moment().locale('en').format('dddd');
             }, 1000)
 
 		}]
