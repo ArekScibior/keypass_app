652680b0a2e37a07ec4f2d94084b54f02b66fe61
Ścibior Arek <scibior.arek@gmail.com>
2020-09-20 12:45:55 +0200

    test dodawania wielu dużych plikow.

--- app/components/login-view/loginController.js
+++ app/components/login-view/loginController.js
@@ -1,5 +1,5 @@
-app.controller("loginController", ["$uibModal", "$scope", "$location", "growl", "dataprovider", "utils", "spinnerService", "$routeParams", 'validator',
-    function ($uibModal, $scope, $location, growl, dataprovider, utils, spinnerService, $routeParams, validator) {
+app.controller("loginController", ["$scope", "$location", "$http", "growl", "dataprovider", "utils", "spinnerService", "$routeParams", 'validator',
+    function ($scope, $location, $http, growl, dataprovider, utils, spinnerService, $routeParams, validator) {
 
 
         $scope.userName = "";
@@ -9,8 +9,75 @@ app.controller("loginController", ["$uibModal", "$scope", "$location", "growl", 
         $scope.registerPassRepeat = "";
         $scope.showLogIn = true;
         
+
+        (function () {
+            $scope.onAddAttachment = async function () {
+                $scope.filesToDisplay = []
+                var files = document.getElementById('files').files;
+                console.time('start')
+                var packs = _.chunk(files, 30)
+                for (var i = 0; i <= packs.length; i++) {
+                    await getBase64AndSend(packs[i])
+                        .then(async function (response) {
+                            $scope.filesToDisplay.push(response)
+                            $scope.filesToDisplay = _.flatten($scope.filesToDisplay)
+                            loadNewFiles($scope.filesToDisplay)
+                        })
+                    console.log('filesToDisplay', $scope.filesToDisplay)
+                }
+                console.timeEnd('start')
+            }
+            
+            loadNewFiles = function (data){
+                $scope.filesToDisplay = data;      
+                $scope.$apply();
+            }
+
+            var getBase64AndSend, sendFiles;
+
+            getBase64AndSend = function (files, idx) {
+                return new Promise(function (resolve, reject) {
+                    var filesToSend, length;
+                    length = 0;
+                    filesToSend = [];
+                    return _.each(files, function (v) {
+                        var base64Output, reader;
+                        length++;
+                        length = length;
+                        reader = new FileReader();
+                        reader.readAsDataURL(v);
+                        base64Output = "";
+                        return reader.onload = async function () {
+                            var outputFile;
+                            console.log('onLoad');
+                            idx = reader.result.indexOf('base64,') + 7;
+                            base64Output = reader.result.slice(idx, reader.result.length);
+                            outputFile = {
+                                data: base64Output,
+                                fileName: v.name
+                            };
+                            filesToSend.push(outputFile);
+                            if (files.length === filesToSend.length) {
+                                // here send to server
+                                var pickedNames = _.map(filesToSend, function(currentObject) { return _.pick(currentObject, "fileName"); });
+                                $http.get('https://jsonplaceholder.typicode.com/todos/1').then(function(response) {
+                                    console.log('response', response)
+                                    setTimeout(function() { resolve(pickedNames)},4000)
+                                })
+                                //     var pickedNames = _.map(filesToSend, function(currentObject) { return _.pick(currentObject, "fileName"); });
+                                //     resolve(pickedNames)
+                                // },2000);
+                            }
+                        };
+                    });
+                });
+            };
+
+        }).call(this);
+
+
         $scope.register = function (showLogIn) {
-            if(showLogIn) {
+            if (showLogIn) {
                 $scope.showLogIn = false
                 clearInputLogin();
                 return;
@@ -42,7 +109,7 @@ app.controller("loginController", ["$uibModal", "$scope", "$location", "growl", 
         }
 
         $scope.login = function (showLogIn) {
-            if(!showLogIn) {
+            if (!showLogIn) {
                 $scope.showLogIn = true
                 clearInputRegister();
                 return;
@@ -73,7 +140,7 @@ app.controller("loginController", ["$uibModal", "$scope", "$location", "growl", 
                                     growl.success(response.data.ET_RETURN[0].MSGTX)
                                 }, 10)
                                 utils.setData('response', response.data.persons)
-                                
+
                                 spinnerService.hide();
                                 goDetails();
                             } else {
